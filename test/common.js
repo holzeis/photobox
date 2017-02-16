fcb245a48804961fbbd8f2a39f1a1f0e785cc0af
Philipp Hoenisch <philipp@hoenisch.at>
2017-02-16 20:12:22 +0100

    fixed imports and extended stub

--- test/common.js
+++ test/common.js
@@ -9,6 +9,24 @@ global.sinon = require('sinon');
 global.sinonChai = require('sinon-chai');
 global.chai.use(global.sinonChai);
 
-global.wiringpiStub = {};
+global.wiringpiStub = {
+    OUTPUT: 'OUTPUT',
+    INPUT: 'INPUT',
+    PUD_UP: 'PUD_UP',
+
+    setup(val) {
+        console.log("wiring stub set up with ", val);
+    },
+    pinMode(pin, mode) {
+        console.log("pin %s setup with mode %s", pin, mode);
+    },
+    pullUpDnControl(pin, mode) {
+        console.log("pin %s setup with mode %s", pin, mode);
+    },
+    wiringPiISR(pin, mode) {
+        console.log("pin %s setup with mode %s", pin, mode);
+    }
+};
+
 global.proxyquire = require('proxyquire').noCallThru();
-proxyquire('../lib/gpioController', { 'wiring-pi': wiringpiStub });
+proxyquire('../lib/gpioController', {'wiring-pi': wiringpiStub});
