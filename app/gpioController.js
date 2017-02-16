b7e9bd6f8cc0440f5a50c2bb7b6727b3266d8714
Philipp Hoenisch <philipp@hoenisch.at>
2017-02-16 16:07:39 +0100

    for simple manual testing

--- app/gpioController.js
+++ app/gpioController.js
@@ -2,7 +2,6 @@
 
 const Logger = require('./logger');
 const GpioPin = require('./GPIOPin');
-//const WiringPiController = require('./wiringPiController');
 
 const wpi = require('wiring-pi');
 
@@ -11,15 +10,17 @@ class GpioController {
 
     constructor (ipAddress) {
         this.logger = new Logger();
-        this.buttonPin = new GpioPin(18, 0);
+        this.buttonPin = new GpioPin(19, 0);
+        this.butonLedPin = new GpioPin(18, 0);
         this.ledPin1 = new GpioPin(13, 0);
         this.ledPin2 = new GpioPin(15, 0);
     }
 
     init() {
-        wpi.wiringPiSetup();
+        wpi.setup('wpi');
         wpi.pinMode(this.ledPin1, wpi.OUTPUT);
         wpi.pinMode(this.ledPin2, wpi.OUTPUT);
+        wpi.pinMode(this.butonLedPin, wpi.OUTPUT);
         wpi.pinMode(this.buttonPin, wpi.INPUT);
     }
 
@@ -28,13 +29,15 @@ class GpioController {
     }
 
     turnLEDOn() {
-        wpi.digitalWrite(ledPin1, ledPin1.status = 1);
-        wpi.digitalWrite(ledPin2, ledPin2.status = 1);
+        wpi.digitalWrite(this.ledPin1, this.ledPin1.status = 1);
+        wpi.digitalWrite(this.ledPin2, this.ledPin2.status = 1);
+        wpi.digitalWrite(this.butonLedPin, this.ledPin2.status = 1);
     }
 
     turnLEDOff() {
-        wpi.digitalWrite(ledPin1, ledPin1.status = 0);
-        wpi.digitalWrite(ledPin2, ledPin2.status = 0);
+        wpi.digitalWrite(this.ledPin1, this.ledPin1.status = 0);
+        wpi.digitalWrite(this.ledPin2, this.ledPin2.status = 0);
+        wpi.digitalWrite(this.butonLedPin, this.butonLedPin.status = 0);
     }
 
 }
--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 
 const CameraController = require('./app/cameraController');
 const Logger = require('./app/logger');
+const GpioController = require('./app/gpioController');
+
 let logger = new Logger();
 
-let c = new CameraController();
-logger.log("1. initializing camera controller");
-c.init();
-logger.log("2. camera status: " + c.getStatus());
-logger.log("3. take picture");
-c.takePhoto();
+// let c = new CameraController();
+// logger.log("1. initializing camera controller");
+// c.init();
+// logger.log("2. camera status: " + c.getStatus());
+// logger.log("3. take picture");
+// c.takePhoto();
+let gpioController = new GpioController();
+gpioController.init();
+
+gpioController.turnLEDOn();
 
