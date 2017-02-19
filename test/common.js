542a6ded75e65506494d60b99bce24f3e25ab9b0
Philipp Hoenisch <philipp@hoenisch.at>
2017-02-19 12:29:57 +0100

    testing using observables and stubs

--- test/common.js
+++ test/common.js
@@ -9,11 +9,17 @@ global.sinon = require('sinon');
 global.sinonChai = require('sinon-chai');
 global.chai.use(global.sinonChai);
 
-global.onoffStub = {
-    Gpio (pin, mode, direction) {
-
+global.onoffStub = {};
+class Gpio {
+    constructor(gpio, direction, edge, options) {
     }
-};
+    watch(callback) {
+        callback(null, "1");
+    }
+    unexport() {
+    }
+}
+global.onoffStub.Gpio = Gpio;
 
 global.proxyquire = require('proxyquire').noCallThru();
 proxyquire('../lib/gpioController', {'onoff': onoffStub});
